c82dc7f42755fc8ea338cee60a388446b6ca8b7f
b21784807 <44296769+b21784807@users.noreply.github.com>
2023-03-18 17:41:24 +0300

    Login Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Card} from 'antd'
+import LoginPage from "./Components/Pages/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card style={{margin:'16px 550px 0px',width:'500px'}}>
+      <LoginPage/>
+    </Card>
   );
 }
 
